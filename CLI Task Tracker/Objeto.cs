0eba9ceb1398e945f3565d204c2df306ad3bdf57
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2024-08-18 20:10:22 -0400

    asdf

---
+++ CLI Task Tracker/Objeto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI_Task_Tracker
+{
+    internal class Objeto
+    {
+        public string Nombre { get; set; }
+        public int Numero { get; set; }
+
+        public Objeto(string Nombre, int Numero)
+        {
+            this.Nombre = Nombre;
+            this.Numero = Numero;
+        }
+    }
+}
--- CLI Task Tracker/Program.cs
+++ CLI Task Tracker/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace CLI_Task_Tracker
+﻿using System.Text.Json;
+
+namespace CLI_Task_Tracker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string json = JsonSerializer.Serialize(new Objeto("Jose", 829567));
+            Console.WriteLine(json);
+
+            Objeto? desjson = JsonSerializer.Deserialize<Objeto>(json);
+
+            //Console.WriteLine(
+            //@$"
+            //Nombre: {desjson.Nombre}
+            //Numero: {desjson.Numero}
+            
+
+            //{desjson}
+            //");
+            string name = "Archivo.json";
+            string[,] array2d =
+            {
+                { JsonSerializer.Serialize(new Objeto("Jose", 829567)), "hola"},
+                {"hola2","hola3"}
+            };
+            //File.OpenWrite(name);
+            //foreach (string n in newjson)
+            //{
+            //    File.AppendText(n);
+            //}
+            //Console.WriteLine(File.ReadAllText(name));
+
+            Console.WriteLine(@$"
+            0,0: {array2d[0, 0]}
+            1,0: {array2d[1, 0]}
+            0,1: {array2d[0, 1]}
+            1,1: {array2d[1, 1]}
+            ");
+
+
+            string[,,] array3d = new string[,,]
+            {
+                {{ JsonSerializer.Serialize(new Objeto("Jose", 829567)), "hola","hola2"} , {"hola3","hola4","hola 5"}},
+                {{"Hola6", "hola7","hola7"} , {"hola9","hola10","hola11"}}
+            };
+
+            Console.WriteLine(@$"
+            0,0,0: {array3d[0, 0, 0]} #json
+            1,0,0: {array3d[1, 0, 0]} #hola6
+            0,1,0: {array3d[0, 1, 0]} #hola3
+            0,0,2: {array3d[0, 0, 1]} #hola2
+            ");
+
         }
     }
 }
