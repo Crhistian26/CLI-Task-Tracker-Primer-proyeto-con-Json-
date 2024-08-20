84f83f1623b9b8b9d24797fb9a96326d49a070f7
Crhistian26 <jhan.cterreroramirez26@gmail.com>
2024-08-19 21:14:35 -0400

    Inicio

--- CLI Task Tracker/Program.cs
+++ CLI Task Tracker/Program.cs
@@ -6,54 +6,30 @@ namespace CLI_Task_Tracker
     {
         static void Main(string[] args)
         {
-
-            string json = JsonSerializer.Serialize(new Objeto("Jose", 829567));
-            Console.WriteLine(json);
-
-            Objeto? desjson = JsonSerializer.Deserialize<Objeto>(json);
-
-            //Console.WriteLine(
-            //@$"
-            //Nombre: {desjson.Nombre}
-            //Numero: {desjson.Numero}
-            
-
-            //{desjson}
-            //");
-            string name = "Archivo.json";
-            string[,] array2d =
+            char? select = null;
+            do
             {
-                { JsonSerializer.Serialize(new Objeto("Jose", 829567)), "hola"},
-                {"hola2","hola3"}
-            };
-            //File.OpenWrite(name);
-            //foreach (string n in newjson)
-            //{
-            //    File.AppendText(n);
-            //}
-            //Console.WriteLine(File.ReadAllText(name));
-
-            Console.WriteLine(@$"
-            0,0: {array2d[0, 0]}
-            1,0: {array2d[1, 0]}
-            0,1: {array2d[0, 1]}
-            1,1: {array2d[1, 1]}
+                Console.WriteLine
+                (@"
+Selecciona una opcion:
+    1- Ver tareas
+    2- Agregar tareas
+    3- Editar tareas
+    4- Eliminar tareas
             ");
 
-
-            string[,,] array3d = new string[,,]
-            {
-                {{ JsonSerializer.Serialize(new Objeto("Jose", 829567)), "hola","hola2"} , {"hola3","hola4","hola 5"}},
-                {{"Hola6", "hola7","hola7"} , {"hola9","hola10","hola11"}}
-            };
-
-            Console.WriteLine(@$"
-            0,0,0: {array3d[0, 0, 0]} #json
-            1,0,0: {array3d[1, 0, 0]} #hola6
-            0,1,0: {array3d[0, 1, 0]} #hola3
-            0,0,2: {array3d[0, 0, 1]} #hola2
-            ");
-
+                select = Console.ReadKey().KeyChar;
+                if (select >= '1' && select <= '4')
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Ingresa el numero correcto.");
+                }
+            }while(select < '1' || select > '4');
+            Console.Clear();
         }
     }
 }
